a162a3560a7be294b56419eb58832e6e3d8cbacf
wilsonvetdev <wilsongch92@gmail.com>
2021-03-02 10:29:57 -0500

    server and request class

--- simulation.py
+++ simulation.py
@@ -1,12 +1,25 @@
 import argparse 
 import csv 
 
+class Server: 
 
+    def greet(self):
+        print('hello')
+
+class Request: 
+
+    def greet(self):
+        print('hello')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", help="csv file path", type=str, required=True)
     parser.add_argument("--servers", help="simulate multiple servers", type=str)
     args = parser.parse_args()
+
     print(args.file)
 
+    server = Server()
+    server.greet()
+    request = Request()
+    request.greet()
