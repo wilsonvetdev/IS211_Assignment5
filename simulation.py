ca6f193a3c53d3c9449f7b85b8f7e608f47c4eb2
wilsonvetdev <wilsongch92@gmail.com>
2021-03-06 22:51:28 -0500

    wip

--- simulation.py
+++ simulation.py
@@ -3,8 +3,10 @@ import csv
 
 class Server: 
 
-    def greet(self):
-        print('hello')
+    def __init__(self):
+        self.current_task = None
+        self.time_remaining = 0
+
 
 class Request: 
 
@@ -13,20 +15,19 @@ class Request:
         self.file_name = file_name
         self.duration = duration 
 
-def process_data(filename):
+
+def simulateOneServer(filename):
     
     with open(filename, newline='') as csv_file:
         data_reader = csv.reader(csv_file, delimiter=',')
         for line in data_reader:
-            print(line)
             new_request = Request(line[0], line[1], line[2])
-            print(new_request.request_time, new_request.file_name, new_request.duration)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", help="csv file path", type=str, required=True)
     parser.add_argument("--servers", help="simulate multiple servers", type=str)
     args = parser.parse_args()
-
-    print(args.file)
-    process_data(args.file)
+    
+    simulateOneServer(args.file)
