e40a8f22aca916c91e3717cc909e721b5d866148
wilsonvetdev <wilsongch92@gmail.com>
2021-03-06 22:08:27 -0500

    able to read every lines from csv, outputs as lists

--- simulation.py
+++ simulation.py
@@ -11,6 +11,13 @@ class Request:
     def greet(self):
         print('hello')
 
+def process_data(filename):
+    
+    with open(filename, newline='') as csv_file:
+        data_reader = csv.reader(csv_file, delimiter=',')
+        for line in data_reader:
+            print(line)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", help="csv file path", type=str, required=True)
@@ -18,6 +25,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print(args.file)
+    process_data(args.file)
 
     server = Server()
     server.greet()
