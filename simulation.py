aa773e4b566900746037ae7ac542f4813d80245a
wilsonvetdev <wilsongch92@gmail.com>
2021-03-07 19:01:06 -0500

    sim wip

--- simulation.py
+++ simulation.py
@@ -3,10 +3,26 @@ import csv
 
 class Server: 
 
-    def __init__(self):
-        self.current_task = None
+    def __init__(self, requests = []):
+        self.current_request = None
+        self.requests = requests
         self.time_remaining = 0
 
+    def tick(self, request):
+        if self.current_request != None:
+            self.time_remaining = self.time_remaining - request.duration
+            if self.time_remaining <= 0:
+                self.current_request = None 
+    
+    def busy(self):
+        if self.current_request != None:
+            return True 
+        else:
+            return False 
+    
+    def start_next(self, new_request):
+        self.current_request = new_request
+        self.time_remaining = new_request.duration
 
 class Request: 
 
@@ -17,12 +33,18 @@ class Request:
 
 
 def simulateOneServer(filename):
-    
-    with open(filename, newline='') as csv_file:
+
+    server = Server()
+
+    with open(filename) as csv_file:
         data_reader = csv.reader(csv_file, delimiter=',')
         for line in data_reader:
             new_request = Request(line[0], line[1], line[2])
+            server.requests.append(new_request)
 
+    while len(server.requests) != 0:
+        server.current_request = server.requests.pop(0)
+        print(server.current_request.request_time, server.current_request.file_name, server.current_request.duration)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
